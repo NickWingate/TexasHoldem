1bc0ea6ba21ac3c13c3fb6e08f43b639fdc715c0
NickWingate <nicholas.wingate03@gmail.com>
2021-04-08 21:57:04 +0100

    Adds ShowCardService.cs

---
+++ TexasHoldem.ConsoleUI/Services/ShowCardService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using TexasHoldem.Domain.Entities;
+
+namespace TexasHoldem.ConsoleUI.Services
+{
+	public class ShowCardService : IShowCardService
+	{
+		public void ShowCards(List<Player> players)
+		{
+			foreach (var player in players)
+			{
+				ShowCards(player);
+			}
+		}
+
+		private void ShowCards(Player player)
+		{
+			Console.Write($"Press enter to see {player.Name}'s cards");
+			Console.ReadLine();
+			foreach (var card in player.Hand)
+			{
+				Console.WriteLine(card.ToString());
+			}
+			Console.WriteLine("Press enter to clear cards from screen");
+			Console.ReadLine();
+            Console.Clear();
+		}
+	}
+}
