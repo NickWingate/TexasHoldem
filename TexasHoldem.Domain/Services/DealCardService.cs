910e8e518164a83e3481a872eee9998651ce664e
NickWingate <nicholas.wingate03@gmail.com>
2021-04-08 21:53:51 +0100

    Adds DealCardService

---
+++ TexasHoldem.Domain/Services/DealCardService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using TexasHoldem.Domain.Entities;
+using TexasHoldem.Domain.Exceptions;
+
+namespace TexasHoldem.Domain.Services
+{
+	public class DealCardService : IDealCardService
+	{
+		public void DealCards(List<Player> players, Deck deck, int cardsPerHand)
+		{
+			CheckForEnoughCards(deck, players.Count, cardsPerHand);
+			for (int i = 0; i < cardsPerHand; i++)
+			{
+				foreach (var player in players)
+				{
+					player.Hand.Add(deck.DrawCard());
+				}
+			}
+		}
+
+		private void CheckForEnoughCards(Deck deck, int playersCount, int cardsPerHand)
+		{
+			var cardsNeeded = playersCount * cardsPerHand;
+			if (cardsNeeded > deck.Count)
+			{
+				throw new EmptyDeckException("There are not enough cards for another round");
+			}
+		}
+	}
+}
---
+++ TexasHoldem.Domain/Services/IDealCardService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using TexasHoldem.Domain.Entities;
+
+namespace TexasHoldem.Domain.Services
+{
+	public interface IDealCardService
+	{
+		void DealCards(List<Player> players, Deck deck, int cardsPerHand);
+	}
+}
