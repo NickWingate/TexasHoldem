63861339686660972d855bd9a71179076cbf35ea
NickWingate <nicholas.wingate03@gmail.com>
2021-05-11 22:51:27 +0100

    Adds unit tests for CardCodeServiceTests.cs

---
+++ TexasHoldem.WpfServices.UnitTests/CardCodeServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using TexasHoldem.Domain.Entities;
+using TexasHoldem.Domain.Enums;
+using TexasHoldem.WpfUI.Services;
+using TexasHoldem.WpfUI.Services.Interfaces;
+using Xunit;
+
+namespace TexasHoldem.WpfServices.UnitTests
+{
+	public class CardCodeServiceTests
+	{
+		private readonly ICardCodeService _sut;
+
+		public CardCodeServiceTests()
+		{
+			_sut = new CardCodeService();
+		}
+
+		[Fact]
+		public void GetCode_ShouldReturnCardCode_WhenEasyCard()
+		{
+			// Arrange
+			var card = new Card(Suit.Diamonds, Rank.Four);
+			var expected = "4D";
+			// Act
+			var cardCode = _sut.GetCode(card);
+
+			// Assert
+			cardCode.Should().BeEquivalentTo(expected);
+		}
+		
+		[Fact]
+		public void GetCode_ShouldReturnCardCode_WhenComplexCard()
+		{
+			// Arrange
+			var card = new Card(Suit.Diamonds, Rank.Jack);
+			var expected = "JD";
+			// Act
+			var cardCode = _sut.GetCode(card);
+
+			// Assert
+			cardCode.Should().BeEquivalentTo(expected);
+		}
+	}
+}
