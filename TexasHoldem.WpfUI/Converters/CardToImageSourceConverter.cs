43d4346f452bf615a8bb5de3e867ccd05f3c99e5
NickWingate <nicholas.wingate03@gmail.com>
2021-05-11 22:47:37 +0100

    Adds converters

---
+++ TexasHoldem.WpfUI/Converters/CardToImageSourceConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media;
+using TexasHoldem.Domain.Entities;
+using TexasHoldem.WpfUI.Services;
+using TexasHoldem.WpfUI.Services.Interfaces;
+
+namespace TexasHoldem.WpfUI.Converters
+{
+	public class CardToImageSourceConverter : IValueConverter
+	{
+		private readonly ICardCodeService _cardCodeService;
+
+		public CardToImageSourceConverter()
+		{
+			_cardCodeService = new CardCodeService();
+		}
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var projectPath = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+			var cardCode = _cardCodeService.GetCode((Card) value);
+			var fileName = $"{projectPath}\\Resources\\Cards\\{cardCode}.png";
+
+			return new ImageSourceConverter().ConvertFrom(fileName);
+		}
+		
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		private string GetCode(Card card)
+		{
+			var rankCode = (int) card.Rank <= 10
+				? ((int) card.Rank).ToString()
+				: card.Rank.ToString()[0].ToString();
+			var suitCode = card.Suit.ToString()[0];
+
+			return $"{rankCode}{suitCode}";
+		}
+	}
+}
---
+++ TexasHoldem.WpfUI/Converters/ListToObservableCollectionConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using TexasHoldem.Domain.Entities;
+
+namespace TexasHoldem.WpfUI.Converters
+{
+    public class ListToObservableCollectionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new ObservableCollection<Card>((List<Card>) value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
