4d3eabbfd4c4e39ad004054007b4d9a7ac99d81f
NickWingate <nicholas.wingate03@gmail.com>
2021-05-11 22:45:58 +0100

    Adds Player Control

---
+++ TexasHoldem.WpfUI/Controls/PlayerControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using TexasHoldem.Domain.Entities;
+
+namespace TexasHoldem.WpfUI.Controls
+{
+	public partial class PlayerControl : UserControl
+	{
+		public PlayerControl()
+		{
+			InitializeComponent();
+		}
+
+
+
+
+        public Player Player
+        {
+            get => (Player)GetValue(PlayerProperty);
+            set => SetValue(PlayerProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Player.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PlayerProperty =
+            DependencyProperty.Register(nameof(Player), typeof(Player), typeof(PlayerControl), new PropertyMetadata(null));
+
+
+
+
+
+
+
+
+    }
+}
