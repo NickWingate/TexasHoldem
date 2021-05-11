a7ccceb0340029810a85d89f6bc881971d15a54b
NickWingate <nicholas.wingate03@gmail.com>
2021-05-11 22:44:56 +0100

    Adds playing card control

---
+++ TexasHoldem.WpfUI/Controls/PlayingCard.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using TexasHoldem.Domain.Entities;
+
+namespace TexasHoldem.WpfUI.Controls
+{
+	public partial class PlayingCard : UserControl
+	{
+		public PlayingCard()
+		{
+			InitializeComponent();
+		}
+
+		public static readonly DependencyProperty CardProperty = DependencyProperty.Register(
+			"Card", typeof(Card), typeof(PlayingCard), new PropertyMetadata(default(Card)));
+
+		public Card Card
+		{
+			get => (Card) GetValue(CardProperty);
+			set => SetValue(CardProperty, value);
+		}
+	}
+}
